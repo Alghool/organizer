284f916f01671a1e508602fa157db504c1901baa
= <=>
2019-02-04 18:19:53 +0200

    init style

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,24 +1,35 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
     <title>Organizer| @yield('title')</title>
     <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 </head>
 <body>
-<div class="container">
-    <div class="row">
-        <div class="col-md-4">
-            @section('sidebar')
-                This is the master sidebar.
-            @show
-        </div>
-        <div class="col-md-8">
-            @section('content')
-                This is the main content.
-            @show
+<div class="app-container">
+    <div class="container app-wrapper">
+        <div class="row">
+            <div id="sidebar" class="col-md-4">
+                @section('sidebar')
+                    This is the master sidebar.
+                @show
+            </div>
+            <div id="main-container" class="col-md-8">
+                @section('content')
+                    This is the main content.
+                @show
+            </div>
         </div>
     </div>
 </div>
+
+    <script
+        src="https://code.jquery.com/jquery-2.2.4.min.js"
+        integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
 </body>
 </html>
